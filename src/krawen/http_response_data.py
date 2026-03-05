5a5defd601a1c9bfc4beacf51602b14b8bb62603
it <ityeri9@gmail.com>
2026-03-06 08:17:19 +0900

    add; HTTPResponseInfo.content_type getter

--- src/krawen/http_response_data.py
+++ src/krawen/http_response_data.py
@@ -12,6 +12,12 @@ class HTTPResponseInfo:
 
     def get_header(self, key: str) -> bytes:
         return self.headers[key.lower()]
+    @property
+    def content_type(self) -> bytes | None:
+        try:
+            return self.get_header("content-type")
+        except KeyError:
+            return None
 
 @dataclass
 class HTTPResponseData:
