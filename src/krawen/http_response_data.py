88f9571d06f0c4a0f704d644f0faa54d76e92ada
it <ityeri9@gmail.com>
2026-03-05 16:22:12 +0900

    edit: Change HttpResponseData.headers to dict[str, bytes]

--- src/krawen/http_response_data.py
+++ src/krawen/http_response_data.py
@@ -1,7 +1,4 @@
 from dataclasses import dataclass
-from enum import Enum
-
-from yarl import URL
 
 from krawen.async_chunked_reader import AsyncChunkedReader
 
@@ -11,7 +8,7 @@ class HttpResponseData:
     http_version: bytes
     status_code: int
     reason: bytes
-    headers: dict[bytes, bytes]
+    headers: dict[str, bytes]
     body: AsyncChunkedReader
 
     def __post_init__(self):
