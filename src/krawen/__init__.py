8971a543327ff26c1355c14da7682b108be9c8a8
it <ityeri9@gmail.com>
2026-03-05 11:17:59 +0900

    add: AsyncChunkedReader

--- src/krawen/__init__.py
+++ src/krawen/__init__.py
@@ -2,10 +2,12 @@ from krawen.crawler import Crawler
 from krawen.server import Server
 from krawen import url_manager
 from krawen import utils
+from krawen import async_chunked_reader
 
 __all__ = [
     'Crawler',
     'Server',
     'url_manager',
-    'utils'
+    'utils',
+    'async_chunked_reader'
 ]
