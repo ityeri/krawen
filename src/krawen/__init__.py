d5687ddc1d251da16027c912b8638b518d9f4607
it <ityeri9@gmail.com>
2026-03-05 14:23:14 +0900

    add: HTTP types

--- src/krawen/__init__.py
+++ src/krawen/__init__.py
@@ -3,11 +3,13 @@ from krawen.server import Server
 from krawen import url_manager
 from krawen import utils
 from krawen import async_chunked_reader
+from krawen import http_types
 
 __all__ = [
     'Crawler',
     'Server',
     'url_manager',
     'utils',
-    'async_chunked_reader'
+    'async_chunked_reader',
+    'http_types'
 ]
---
+++ src/krawen/http_types.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from yarl import URL
+
+from krawen.async_chunked_reader import AsyncChunkedReader
+
+
+@dataclass
+class HttpResponseData:
+    http_version: bytes
+    status_code: int
+    reason: bytes
+    headers: dict[bytes, bytes]
+    body: AsyncChunkedReader
+
+@dataclass
+class HttpPath:
+    method: MethodType
+    url: URL
+
+class MethodType(Enum):
+    GET = 'GET',
+    HEAD = 'HEAD',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE'
+    CONNECT = 'CONNECT'
+    OPTIONS = 'OPTIONS'
+    TRACE = 'TRACE'
+    PATCH = 'PATCH'
+
+    @classmethod
+    def from_name(cls, name: str) -> MethodType:
+        for method in MethodType:
+            if method.value == name:
+                return method
+
+        raise ValueError(f'Passed name "{name}" is invalid')
