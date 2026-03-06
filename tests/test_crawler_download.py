b1e5cc217fb288e963759c9413af116458a1042a
--global <ityeri9@gmail.com>
2026-03-06 10:44:15 +0900

    add: Base crawler download test

---
+++ tests/test_crawler_download.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+
+from yarl import URL
+
+from krawen import Crawler, EndpointPath, HTTPMethod
+from krawen.async_file_store import AsyncLocalFileStore
+from krawen.endpoint_store import JsonEndpointStore
+
+os.makedirs('./run/store', exist_ok=True)
+
+file_store = AsyncLocalFileStore('./run/store')
+endpoint_store = JsonEndpointStore('./run/endpoints.json', file_store=file_store)
+
+crawler = Crawler(endpoint_store=endpoint_store)
+
+async def main():
+    async with crawler:
+        endpoint_path = EndpointPath(
+            url=URL('https://example.com'),
+            method=HTTPMethod.GET
+        )
+
+        await crawler.download(endpoint_path)
+
+    await endpoint_store.save()
+
+if __name__ == '__main__':
+    asyncio.run(main())
