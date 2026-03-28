c740d25599792be6ae0699f17c1ed1027b98f0a9
--replace-all <ityeri9@gmail.com>
2026-03-29 07:07:31 +0900

    refactor: Change min python version to 3.12

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
+# krawen
+
+`krawen` is a library for convenient crawling, mirroring.
+Primarily, it consists of `KrawenCrawler`, `KrawenMirrorServer`, and `KrawenCrawlerRunner`.
+
+# installation
+TODO pypi distribution (but it can install easily!)
+
+```shell
+pip install git+https://github.com/ityeri/krawen
 ```
-Do you wanna some crawling?
-```
+
+
+# features
+
+## `KrawenCrawler`
+it's a central crawling feature.
+It provides features to download the full response of a specific endpoint,
+simulate a page network request, and retrieve the sub-redirection links of a page.
+Finally, it saves all data to the ingested `EndpointStore`
+
+## `KrawenMirrorServer`
+it's a mirroring server.
+It hosts the crawled data as a web
+
+## `KrawenCrawlerRunner`
+it's a recursive crawling runner.
+From the seed endpoint path, it's crawling recursively forever
+
+
+# runs
+
--- src/krawen/endpoint_path.py
+++ src/krawen/endpoint_path.py
@@ -1,22 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Self
 
 from yarl import URL
 
-
-@dataclass
-class EndpointPath:
-    url: URL
-    method: HTTPMethod
-
-    def __post_init__(self):
-        if not self.url.path.endswith('/'):
-            self.url = self.url / ''
-
-        return self.url.with_path(self.url.path + '/')
-
-    def __hash__(self):
-        return hash((self.url, self.method))
 
 class HTTPMethod(str, Enum):
     GET = 'GET'
@@ -32,9 +19,23 @@ class HTTPMethod(str, Enum):
     def __str__(self): return self.value
 
     @classmethod
-    def from_name(cls, name: str) -> HTTPMethod:
+    def from_name(cls, name: str) -> Self:
         for method in HTTPMethod:
             if method.value == name:
                 return method
 
         raise ValueError(f'Passed name "{name}" is invalid')
+
+@dataclass
+class EndpointPath:
+    url: URL
+    method: HTTPMethod
+
+    def __post_init__(self):
+        if not self.url.path.endswith('/'):
+            self.url = self.url / ''
+
+        return self.url.with_path(self.url.path + '/')
+
+    def __hash__(self):
+        return hash((self.url, self.method))
